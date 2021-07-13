410ca8e5872cd4ee1838634335c1e78e66f44744
HusseinRizkana <hussein.rizkana@gmail.com>
2021-07-13 18:32:00 +0200

    copy made

--- main.py
+++ main.py
@@ -13,9 +13,7 @@ train_transform = torchvision.transforms.Compose([
         brightness=0.4, contrast=0.4, saturation=0.4, hue=0.4),
     torchvision.transforms.RandomHorizontalFlip(p=0.5),
     torchvision.transforms.RandomVerticalFlip(p=0.5),
-    # torchvision.transforms.RandomResizedCrop([224,224]),
     torchvision.transforms.GaussianBlur((5, 9), sigma=(0.1, 2.0)),
-    #torchvision.transforms.RandomErasing(p=0.5, scale=(0.02, 0.33), ratio=(0.3, 3.3), value=0, inplace=False),
     torchvision.transforms.RandomRotation(30),
     torchvision.transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
 
